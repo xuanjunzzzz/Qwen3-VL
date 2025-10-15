d20c5d836f32e5fe86e60462cead399b97f84678
JJJYmmm <92386084+JJJYmmm@users.noreply.github.com>
2025-10-15 11:41:22 +0800

    save chat_template properly

--- qwen-vl-finetune/qwenvl/train/train_qwen.py
+++ qwen-vl-finetune/qwenvl/train/train_qwen.py
@@ -176,11 +176,12 @@ def train(attn_implementation="flash_attention_2"):
     else:
         trainer.train()
     trainer.save_state()
-    processor.save_pretrained(training_args.output_dir)
 
     model.config.use_cache = True
-
+    
     safe_save_model_for_hf_trainer(trainer=trainer, output_dir=training_args.output_dir)
+    
+    processor.save_pretrained(training_args.output_dir)
 
 
 if __name__ == "__main__":
